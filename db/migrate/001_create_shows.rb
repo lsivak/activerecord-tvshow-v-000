d2aeb9d19c6b9caeafad053a595d8c70ddd89f73
Lara Sanders <larasanders@gmail.com>
2017-12-06 18:19:16 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,27 @@
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.integer :rating
+    end
+  end
+
+rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
+law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
+the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
+fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
+
+
+  def highest_rating
+    Show.maximum(:string)
+  end
+
+  def most_popular_show
+  end
+
+  def lowest_rating
+    Show.minimum(:rating)
+  end
+end
