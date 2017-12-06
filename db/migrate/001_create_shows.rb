1573d2636010dcd5849acabeb7082802097ca586
Lara Sanders <larasanders@gmail.com>
2017-12-06 18:14:18 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -15,10 +15,6 @@ fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "S
 
 
   def highest_rating
-    rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
-    law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
-    the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
-    fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
     Show.maximum(:string)
   end
 
