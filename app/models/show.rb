edc0a9b3ca2219bd4c863a781da1a2f7a26728c3
Lara Sanders <larasanders@gmail.com>
2017-12-06 19:21:03 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,6 +11,6 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end
   def self.least_popular_show
-    Show.where("rating = ?", lowest_rating).flatten
+    Show.where("rating = ?", lowest_rating).flatten.to_s
   end
 end
