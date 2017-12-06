157093646444a8e1bc34ea5035b75114cc131389
Lara Sanders <larasanders@gmail.com>
2017-12-06 18:12:45 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -7,7 +7,7 @@ class CreateShows < ActiveRecord::Migration
       s.integer :rating
     end
   end
-end
+
 rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
 law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
 the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
@@ -24,3 +24,4 @@ fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "S
   def lowest_rating
     Show.minimum(:rating)
   end
+end
