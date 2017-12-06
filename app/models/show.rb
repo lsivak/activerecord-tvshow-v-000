e183dd5d8416e070a21502fc191b18a283719b26
Lara Sanders <larasanders@gmail.com>
2017-12-06 18:06:06 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,3 @@
 class Show < ActiveRecord::Base
 end
 
-
-  def highest_rating
-    Show.maximum(:string)
-  end
-
-  def most_popular_show
-  end
-
-  def lowest_rating
-    Show.minimum(:rating)
-  end
