be04dc7eb09bed7e5332ad55f712e72bca4a7040
Lara Sanders <larasanders@gmail.com>
2017-12-06 17:49:25 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -13,7 +13,7 @@ law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network =
 the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
 fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
 
-def highest_rating
-  Show.maximum(:rating)
-end
+
+
+def most_popular_show
 end
