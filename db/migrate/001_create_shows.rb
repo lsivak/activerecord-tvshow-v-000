6f92f9d14917c291bc0c20f5e5782b94e34fac09
Lara Sanders <larasanders@gmail.com>
2017-12-06 17:56:55 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -7,7 +7,7 @@ class CreateShows < ActiveRecord::Migration
       s.integer :rating
     end
   end
-
+end
 rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
 law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
 the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
